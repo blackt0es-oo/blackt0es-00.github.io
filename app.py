3723f0fc667c685b8a2511ddcd43df5ef2c32816
blackt0es-oo <diah1@outlook.com>
2025-05-05 13:00:59 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template
+
+
+
+app=Flask(__name__)
+history = []
+
+class Task:
+    def __init__(self, action, due):
+        self.action=action
+        self.due=due
+    
+    def format(self):
+        return [self.action,self.due]
+
+
+
+
+@app.route('/', methods=['GET','POST'])
+def backend():
+    global history
+    
+    if request.method == 'POST':
+        action = request.form['action']
+        due = request.form['due']
+        task=Task(action=action, due=due).format()
+        history.append(task)
+        
+    
+    print(history)       
+    return render_template('home.html', history=history)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
